a3760f5dc86b100ddccf8f53de2416b7fd704341
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-03-23 22:04:13 +0900

    create04ex

---
+++ 04ex/04-1ex/ex04-1.js
@@ -0,0 +1,13 @@
+function cointoss() {
+  const num = Math.random() // numは0.0〜1.0のランダムな数値
+
+  if (num >= 0.5) {
+      console.log("表");
+  } else {
+      console.log("裏");
+  }
+}
+
+for (let i = 1; i <= 10; i++) {
+  cointoss(i)
+}
