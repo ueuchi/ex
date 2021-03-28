d75a7e3c8d8c9d1c52a3e8b29f74ed583d770460
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-03-28 19:03:29 +0900

    create08-2

--- 08ex/08-2ex/ex08-2.js
+++ 08ex/08-2ex/ex08-2.js
@@ -1,3 +1,7 @@
-const newItem = document.createElement('li')
-newItem.textContent = 'もも'
-list.appendChild(newItem)
+document.write('<h1>DOM</h1>');
+document.write('<p>JavaScriptからHTMLを扱うための仕組み</p>');
+document.write('<h2>印象に残っているトピック</h2>');
+document.write('<ul><li>documentオブジェクト</li><li>getElementById</li><li>イベントリスナ</li></ul>');
+    
+    
+    
