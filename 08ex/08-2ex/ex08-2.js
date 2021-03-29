cae712669aad025dcbf58464ad8b685672046722
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-03-30 07:07:59 +0900

    exo8-2　修正

--- 08ex/08-2ex/ex08-2.js
+++ 08ex/08-2ex/ex08-2.js
@@ -1,7 +1,28 @@
-document.write('<h1>DOM</h1>');
-document.write('<p>JavaScriptからHTMLを扱うための仕組み</p>');
-document.write('<h2>印象に残っているトピック</h2>');
-document.write('<ul><li>documentオブジェクト</li><li>getElementById</li><li>イベントリスナ</li></ul>');
-    
-    
+const newItem1 = document.createElement('h1')
+newItem1.textContent = 'DOM'
+document.body.appendChild(newItem1)
+
+const newItem2 = document.createElement('p')
+newItem2.textContent = 'JavaScriptからHTMLを扱うための仕組み'
+document.body.appendChild(newItem2)
+
+const newItem3 = document.createElement('h2')
+newItem3.textContent = '印象に残っているトピック'
+document.body.appendChild(newItem3)
+
+const newItem4 = document.createElement('ul')
+document.body.appendChild(newItem4)
+
+const newItem5 = document.createElement('li')
+newItem5.textContent = 'documentオブジェクト'
+document.querySelector('ul').appendChild(newItem5)
+
+const newItem6 = document.createElement('li')
+newItem6.textContent = 'getElementById'
+document.querySelector('ul').appendChild(newItem6)
+
+const newItem7 = document.createElement('li')
+newItem7.textContent = 'イベントリスナ'
+document.querySelector('ul').appendChild(newItem7)
+
     
