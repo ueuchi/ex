b5a9ea56706e223c498bdd445fc2ba961eac34aa
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-04-01 21:20:37 +0900

    commit 10ex

--- 10ex/my-app/src/index.js
+++ 10ex/my-app/src/index.js
@@ -1,8 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.js'
+import React from 'react'
+import App from './App'
 
-ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
-)
+ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('root'))
