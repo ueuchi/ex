801130aa0a4f014d17f5b4579a4b0a6db6cb30ea
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-05-19 05:58:44 +0900

    修正

--- chat-app/src/pages/Login.jsx
+++ chat-app/src/pages/Login.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import { AuthContext } from '../AuthService'
 import firebase from '../config/firebase'
 
-const Login = () => {
+const Login = ({history}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(() => {
+                history.push("/")
+            })
             .catch(err => {
                 console.log(err)
-            }) 
+            })
     }
+
+    const user = useContext(AuthContext)
+
+    if(user) {
+        return<Redirect to="/" />
+    }
+
     return (
         <>
             <h1>Login</h1>        
--- chat-app/src/pages/Room.jsx
+++ chat-app/src/pages/Room.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Login from './Login'
 import SignUp from './SignUp'
+import firebase from '../config/firebase'
 
 const Room = () => {
+    const [messages, setMessages] = useState(null)
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        firebase.firestore().collection('messages')
+            .onSnapshot((snapshot) => {
+                const messages = snapshot.docs.map(doc => {
+                    return doc.data()
+                })
+
+                setMessages(messages)
+            })
+    }, [])
+    
     return (
         <>
             <h1>Room</h1>
+            <ul>
+                <li>
+                    sample user : sample message
+                </li>
+            </ul>
+            <form>
+                <input 
+                    type="text" 
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+                <button type="submit">送信</button>
+            </form>
+            <button onClick={() => firebase.auth().signOut()}>Logout</button>
         </>
     )
 }
